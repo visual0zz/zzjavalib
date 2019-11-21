05cbebd9f39a59eeea81d178bb412023aa83f32b
zhaozhi_bjb <indiancarriernoac@qq.com>
2019-11-21 15:45:52 +0800

    预计算质数表

--- src/main/java/com/zz/utils/threadsafe/math/prime/PrimeNumberGenerator.java
+++ src/main/java/com/zz/utils/threadsafe/math/prime/PrimeNumberGenerator.java
@@ -1,12 +1,21 @@
 package com.zz.utils.threadsafe.math.prime;
 
+import java.math.BigInteger;
+
 public class PrimeNumberGenerator{
     private PrimeNumberGenerator(){}
-    private static PrimeNumberGenerator generator;
+    private static PrimeNumberGenerator generator=new PrimeNumberGenerator();
     public PrimeNumberGenerator getInstance(){//构造单例
-        if(generator==null)
-            generator=new PrimeNumberGenerator();
         return generator;
     }
 
+    /**
+     * 获得第index个质数 2是第一个质数，1定义为第0个质数
+     * 即[0,1,2,3,4 , 5,6]=[1,2,3,5,7 , 11,13]
+     * @param index
+     * @return
+     */
+    public BigInteger getPrimeAt(BigInteger index){
+        return null;
+    }
 }
---
+++ src/test/java/com/zz/utils/threadsafe/math/prime/PrimeTableTest.java
@@ -0,0 +1,14 @@
+package com.zz.utils.threadsafe.math.prime;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+public class PrimeTableTest {
+
+    @Test
+    public void calculateBasicPrimeNumbers() {
+        PrimeTable.calculateBasicPrimeNumbers();
+    }
+}
