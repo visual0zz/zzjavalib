1e0cf1d55964d252596bf43953cda68f52f34de0
zhaozhi_bjb <indiancarriernoac@qq.com>
2020-03-22 22:45:01 +0800

    ...

--- src/main/java/com/zz/utils/threadunsafe/storage/impl/GitRepoDatabase.java
+++ src/main/java/com/zz/utils/threadunsafe/storage/impl/GitRepoDatabase.java
@@ -1,16 +1,23 @@
 package com.zz.utils.threadunsafe.storage.impl;
 
 import com.zz.utils.threadunsafe.storage.Database;
+import com.zz.utils.threadunsafe.storage.DatabaseException;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileSystemException;
 import java.sql.DatabaseMetaData;
 
 class GitRepoDatabase implements Database {
     Git repo;
     public GitRepoDatabase(File baseFolder){
-        Git.lsRemoteRepository().setRemote("");
+        try {
+            repo=Git.init().setDirectory(baseFolder).call();
+        } catch (GitAPIException e) {
+            throw new DatabaseException("GitRepoDatabase open repo failed.",e);
+        }
     }
     @Override
     public String get(String key) {
--- src/test/java/ExploreMechanism.java
+++ src/test/java/ExploreMechanism.java
@@ -1,11 +1,21 @@
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Ref;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
 import java.util.Properties;
 
 public class ExploreMechanism {
     @Test
-    public void testJGit(){
-        Properties p=System.getProperties();
-        p.list(System.out);
+    public void testJGit() throws GitAPIException, IOException {
+        Collection<Ref> refs=Git.lsRemoteRepository().setRemote(".\\test\\repo").call();
+        for (Ref ref:refs){
+            System.out.println(ref);
+        }
+        Git git=Git.init().setDirectory(new File(".\\test\\repo")).call();
+        System.out.println(git.status().call().getUntracked());
     }
 }
