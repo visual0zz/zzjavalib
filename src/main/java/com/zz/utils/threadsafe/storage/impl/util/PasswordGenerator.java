530d44b1e490d9bf5f5e764705dbb32a1ac669e7
zhaozhi_bjb <indiancarriernoac@qq.com>
2020-04-01 22:52:52 +0800

    完成Base16编码

--- src/main/java/com/zz/utils/threadsafe/storage/impl/util/PasswordGenerator.java
+++ src/main/java/com/zz/utils/threadsafe/storage/impl/util/PasswordGenerator.java
@@ -1,24 +1,16 @@
 package com.zz.utils.threadsafe.storage.impl.util;
 
+import java.util.Random;
+
 public class PasswordGenerator {
-    private static final char[] codeTableH={//高半字节使用的表
+    private static final char[] codeTable={
             'a','c','d','e','f','g','h','j',
-            'k','m','n','p','q','r','t','w'};
-    private static final char[] codeTableL={//低半字节使用的表
+            'k','m','n','p','q','r','t','w',
             'x','y','3','4','6','7','9','#',
             '*','!','+','-','?','>','<','.'
     };
-    public static String encode(byte[]data){
-        assert data!=null;
-        StringBuilder builder=new StringBuilder();
-        for(byte b:data){
-            byte h= (byte) ((b>>4)&0x0f);//高位在前低位在后
-            byte l= (byte) (b&0x0f);
-            char hc=codeTableH[h];
-            char lc=codeTableL[l];
-            builder.append(hc);
-            builder.append(lc);
-        }
-        return builder.toString();
+    public void get(){
+        Random random=new Random();
     }
+
 }
