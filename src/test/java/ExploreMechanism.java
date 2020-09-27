73e7d568e07117ec85386f8d7bcd3f5a797c4325
zhaozhi_bjb <indiancarriernoac@qq.com>
2020-09-27 21:09:54 +0800

    删除关于jgit的测试代码

--- src/test/java/ExploreMechanism.java
+++ src/test/java/ExploreMechanism.java
@@ -1,12 +1,6 @@
 
-import com.zz.utils.threadsafe.cryptography.SboxAES;
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.Properties;
 
 public class ExploreMechanism {
     @Test
