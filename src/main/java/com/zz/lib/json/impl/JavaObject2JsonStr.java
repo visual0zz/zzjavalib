1456bc074ae16662090ca5410f6064f5581b0b2d
赵治 <zhaozhi@mszq.com>
2023-02-23 12:27:56 +0800

    Java to Json 字符串转义功能完成

--- src/main/java/com/zz/lib/json/impl/JavaObject2JsonStr.java
+++ src/main/java/com/zz/lib/json/impl/JavaObject2JsonStr.java
@@ -5,145 +5,211 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class JavaObject2JsonStr{
-    private static JavaObject2JsonStr BUILDER=new JavaObject2JsonStr(false);
-    private static JavaObject2JsonStr FORMATTED_BUILDER=new JavaObject2JsonStr(false);
+public class JavaObject2JsonStr {
+    private static JavaObject2JsonStr BUILDER = new JavaObject2JsonStr(false);
+    private static JavaObject2JsonStr FORMATTED_BUILDER = new JavaObject2JsonStr(false);
 
-    JavaObject2JsonStr(boolean formatted){
-        this.formatted=formatted;
+    JavaObject2JsonStr(boolean formatted) {
+        this.formatted = formatted;
     }
-    public static String toJson(Object object){
-        return BUILDER.construct(object,1);
+
+    public static String toJson(Object object) {
+        return BUILDER.construct(object, 1);
     }
-    public static String toFormatJson(Object object){
-        return FORMATTED_BUILDER.construct(object,1);
+
+    public static String toFormatJson(Object object) {
+        return FORMATTED_BUILDER.construct(object, 1);
     }
+
     boolean formatted;
-    String construct(Object object,int level){
-        if(object==null){
+
+    String construct(Object object, int level) {
+        if (object == null) {
             return null;
-        }else if(object instanceof Integer){
-            return String.valueOf((Integer) object);
-        }else if(object instanceof Long){
-            return String.valueOf((Long) object);
-        }else if(object instanceof BigDecimal){
+        } else if (object instanceof Integer || object instanceof Long || object instanceof Short || object instanceof Byte) {
+            return String.valueOf(object);
+        } else if (object instanceof BigDecimal) {
             return constructDecimal((BigDecimal) object);
-        }else if(object instanceof List){
-            return constructArray((List<Object>) object,level);
-        }else if(object instanceof Map){
-            return constructMap((Map<String, Object>) object,level);
-        }else if(object instanceof byte[]){
-            List<Object> array=new ArrayList<>();
-            for(byte b:(byte[])object){
-                array.add((int)b);
+        } else if (object instanceof Float) {
+            return constructDecimal(BigDecimal.valueOf((Float) object));
+        } else if (object instanceof Double) {
+            return constructDecimal(BigDecimal.valueOf((Double) object));
+        } else if (object instanceof List) {
+            return constructArray((List<Object>) object, level);
+        } else if (object instanceof Map) {
+            return constructMap((Map<String, Object>) object, level);
+        } else if (object instanceof byte[]) {
+            List<Object> array = new ArrayList<>();
+            for (byte b : (byte[]) object) {
+                array.add((int) b);
             }
-            return constructArray(array,level);
-        }else if(object instanceof short[]){
-            List<Object> array=new ArrayList<>();
-            for(short b:(short[])object){
-                array.add((int)b);
+            return constructArray(array, level);
+        } else if (object instanceof short[]) {
+            List<Object> array = new ArrayList<>();
+            for (short b : (short[]) object) {
+                array.add((int) b);
             }
-            return constructArray(array,level);
-        }else if(object instanceof int[]){
-            List<Object> array=new ArrayList<>();
-            for(int b:(int[])object){
+            return constructArray(array, level);
+        } else if (object instanceof int[]) {
+            List<Object> array = new ArrayList<>();
+            for (int b : (int[]) object) {
                 array.add(b);
             }
-            return constructArray(array,level);
-        }else if(object instanceof long[]){
-            List<Object> array=new ArrayList<>();
-            for(long b:(long[])object){
+            return constructArray(array, level);
+        } else if (object instanceof long[]) {
+            List<Object> array = new ArrayList<>();
+            for (long b : (long[]) object) {
                 array.add(b);
             }
-            return constructArray(array,level);
-        }else if(object instanceof float[]){
-            List<Object> array=new ArrayList<>();
-            for(float b:(float[])object){
+            return constructArray(array, level);
+        } else if (object instanceof float[]) {
+            List<Object> array = new ArrayList<>();
+            for (float b : (float[]) object) {
                 array.add(BigDecimal.valueOf(b));
             }
-            return constructArray(array,level);
-        }else if(object instanceof double[]){
-            List<Object> array=new ArrayList<>();
-            for(double b:(double[])object){
+            return constructArray(array, level);
+        } else if (object instanceof double[]) {
+            List<Object> array = new ArrayList<>();
+            for (double b : (double[]) object) {
                 array.add(BigDecimal.valueOf(b));
             }
-            return constructArray(array,level);
-        }else if(object instanceof char[]){
-            return constructString(String.valueOf((char[])object));
-        }else if(object instanceof String){
-            //todo 编码字符串，进行特殊字符的转义
-            return constructString((String) object);
+            return constructArray(array, level);
+        } else if (object instanceof char[]) {
+            return constructString(String.valueOf((char[]) object));
+        } else if (object instanceof Character[]) {
+            StringBuilder builder = new StringBuilder();
+            for (Character c : (Character[]) object) {
+                builder.append(c);
+            }
+            return constructString(builder.toString());
+        } else if (object instanceof String || object instanceof Character) {
+            return constructString("" + object);
         }
         //todo 判断对象是直接继承Object的简单对象，直接提取内部属性
         return "";
     }
-    String constructArray(List<Object> array, int level){
-        StringBuilder result=new StringBuilder();
+
+    String constructArray(List<Object> array, int level) {
+        StringBuilder result = new StringBuilder();
         result.append("[");
-        if(formatted){
+        if (formatted) {
             result.append("\n");
         }
-        for(int i=0;i<array.size();i++){
-            Object obj=array.get(i);
-            if(obj!=null){
-                leadingSpace(result,level);
-                result.append(construct(obj,level+1));
-                if(i<array.size()-1){
+        for (int i = 0; i < array.size(); i++) {
+            Object obj = array.get(i);
+            if (obj != null) {
+                leadingSpace(result, level);
+                result.append(construct(obj, level + 1));
+                if (i < array.size() - 1) {
                     result.append(',');
                 }
-                if(formatted){
+                if (formatted) {
                     result.append("\n");
                 }
             }
         }
-        leadingSpace(result,level-1);
+        leadingSpace(result, level - 1);
         result.append(']');
         return result.toString();
     }
-    String constructMap(Map<String,Object> map, int level){
-        StringBuilder result=new StringBuilder();
+
+    String constructMap(Map<String, Object> map, int level) {
+        StringBuilder result = new StringBuilder();
         result.append("{");
-        if(formatted){
+        if (formatted) {
             result.append("\n");
         }
-        for(Map.Entry<String,Object> entry:map.entrySet()){
-            if(entry.getValue()!=null){
-                leadingSpace(result,level);
-                result.append('"')
-                        .append(entry.getKey())
-                        .append("\": ")
-                        .append(construct(entry.getValue(),level+1))
-                        .append(',');
-                if(formatted){
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            if (entry.getValue() != null) {
+                leadingSpace(result, level);
+                result.append('"').append(entry.getKey()).append("\": ").append(construct(entry.getValue(), level + 1)).append(',');
+                if (formatted) {
                     result.append("\n");
                 }
             }
         }
-        if(result.charAt(result.length()-1)==','){
-            result.deleteCharAt(result.length()-1);
+        if (result.charAt(result.length() - 1) == ',') {
+            result.deleteCharAt(result.length() - 1);
         }
         return result.toString();
     }
 
-    String constructDecimal(BigDecimal number){
-        number=number.stripTrailingZeros();
-        StringBuilder result=new StringBuilder();
-        if(number.abs().compareTo(new BigDecimal("0.1"))>0
-                && number.abs().compareTo(new BigDecimal("1e8"))<0 ){
+    String constructDecimal(BigDecimal number) {
+        number = number.stripTrailingZeros();
+        StringBuilder result = new StringBuilder();
+        if (number.abs().compareTo(new BigDecimal("0.00001")) > 0 && number.abs().compareTo(new BigDecimal("1e5")) < 0 || number.compareTo(new BigDecimal("0")) == 0) {
             result.append(number.toPlainString());
-        }else{
-            result.append(number);
+        } else {
+            result.append(number.toEngineeringString());
         }
         return result.toString();
     }
-    String constructString(String str){
-        return '"' + str + '"';
+
+    String constructString(String str) {
+        StringBuilder s=new StringBuilder();
+        if (str == null || str.isEmpty()) {
+            s.append("\"\"");
+            return s.toString();
+        }
+
+        char c; // 当前字符
+        int len = str.length();
+        s.append('"');
+
+        for (int i = 0; i < len; i++) {
+            c = str.charAt(i);
+            switch (c) {
+                case '\\':
+                case '"':
+                    s.append("\\");
+                    s.append(c);
+                    break;
+                default:
+                    String result;
+                    switch (c) {
+                        case '\b':
+                            result = "\\b";
+                            break;
+                        case '\t':
+                            result = "\\t";
+                            break;
+                        case '\n':
+                            result = "\\n";
+                            break;
+                        case '\f':
+                            result = "\\f";
+                            break;
+                        case '\r':
+                            result = "\\r";
+                            break;
+                        default:
+                            if (c < ' ' || //
+                                    (c >= '\u0080' && c <= '\u00a0') || //
+                                    (c >= '\u2000' && c <= '\u2010') || //
+                                    (c >= '\u2028' && c <= '\u202F') || //
+                                    (c >= '\u2066' && c <= '\u206F')//
+                            ) {
+                                result = "\\u" + Integer.toString(c, 16);
+                                break;
+                            } else {
+                                result = Character.toString(c);
+                                break;
+                            }
+                    }
+                    s.append(result);
+            }
+        }
+        s.append('"');
+
+        return s.toString();
     }
-    private void leadingSpace(StringBuilder builder,int level){
-        if(formatted){
-            for(int i=0;i<level;i++){
+
+    private void leadingSpace(StringBuilder builder, int level) {
+        if (formatted) {
+            for (int i = 0; i < level; i++) {
                 builder.append("  ");
             }
         }
     }
+
 }
--- src/test/java/com/zz/lib/json/impl/JavaObject2JsonStrTest.java
+++ src/test/java/com/zz/lib/json/impl/JavaObject2JsonStrTest.java
@@ -3,6 +3,8 @@ package com.zz.lib.json.impl;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 public class JavaObject2JsonStrTest {
 
     @Test
@@ -35,6 +37,27 @@ public class JavaObject2JsonStrTest {
         Assert.assertEquals("[\n  1,\n  2,\n  3\n]",builder.construct(new int[]{1,2,3},1));
         builder=new JavaObject2JsonStr(true);
         Assert.assertEquals("[\n    1,\n    2,\n    3\n  ]",builder.construct(new int[]{1,2,3},2));
+    }
+    @Test
+    public void constructNumber(){
+        JavaObject2JsonStr builder=new JavaObject2JsonStr(false);
+        Assert.assertEquals("10000",builder.construct(new BigDecimal("1e4"),1));
+        Assert.assertEquals("100E+6",builder.construct(new BigDecimal("1e8"),2));
+        Assert.assertEquals("10E+6",builder.construct(new BigDecimal("1e7"),3));
+        Assert.assertEquals("0.1",builder.construct(new BigDecimal("1e-1"),3));
+        Assert.assertEquals("0.2323",builder.construct(new BigDecimal("0.232300000"),3));
+        Assert.assertEquals("12E-9",builder.construct(new BigDecimal("1.2e-8"),3));
+        builder=new JavaObject2JsonStr(true);
+        Assert.assertEquals("0",builder.construct(new BigDecimal("0"),1));
+        Assert.assertEquals("100E+6",builder.construct(new BigDecimal("1e8"),2));
+        Assert.assertEquals("10E+6",builder.construct(new BigDecimal("1e7"),3));
+        Assert.assertEquals("0.1",builder.construct(new BigDecimal("1e-1"),3));
+        Assert.assertEquals("0.2323",builder.construct(new BigDecimal("000.2323"),3));
+        Assert.assertEquals("12E-9",builder.construct(new BigDecimal("1.2e-8"),3));
+
+        Assert.assertEquals("123",builder.construct(123,1));
+        Assert.assertEquals("123",builder.construct(123L,1));
+        Assert.assertEquals("123",builder.construct(123.0,1));
     }
 
 }
