13ca4e5d9745f794c9ce384b2314667054ad9b5b
赵治 <zhaozhi@mszq.com>
2023-04-21 14:37:06 +0800

    ...

--- src/main/java/com/zz/lib/idealogy/core/World.java
+++ src/main/java/com/zz/lib/idealogy/core/World.java
@@ -1,24 +1,15 @@
 package com.zz.lib.idealogy.core;
-
 import com.zz.lib.idealogy.interfaces.DropStrategy;
-
 import java.util.List;
-
 public interface World {
     void put(Thing thing);
-
     void drop(Id id);
-
     List<Thing> searchThings(Attribute attribute);
-
     <T extends Attribute> List<Thing> searchThings(AttributeFilter<T> filter);
-
+    <T extends Attribute> List<Thing> searchAction(AttributeFilter<T> filter);
     <T extends Attribute> boolean addAction(Action action, AttributeFilter<T> filter);
-
     <T extends Attribute> boolean addAction(Action action, AttributeFilter<T> filter, long tickDelay);
-
     <T extends Attribute> void cancelAction(AttributeFilter<T> filter);
-
     void tick(DropStrategy dropStrategy);
 
 }
