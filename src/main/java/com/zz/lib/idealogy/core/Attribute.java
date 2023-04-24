89c21d8d2ceff924c0e1a848c3272ae0325e3cdf
赵治 <zhaozhi@mszq.com>
2023-04-24 17:26:49 +0800

    copy 默认实现

--- src/main/java/com/zz/lib/idealogy/core/Attribute.java
+++ src/main/java/com/zz/lib/idealogy/core/Attribute.java
@@ -1,7 +1,30 @@
 package com.zz.lib.idealogy.core;
 import com.zz.lib.common.tags.ReadOnly;
+
+import java.lang.reflect.Field;
+
 @ReadOnly
 public interface Attribute {
-    Object getValue(Object key);
-    <T extends Attribute> T copy();
+    default Object getValue(Object key){
+        try{
+            Field field=this.getClass().getDeclaredField((String) key);
+            field.setAccessible(true);
+            return field.get(this);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+    default <T extends Attribute> T copy(){
+        try{
+            T result= (T) this.getClass().getDeclaredConstructor().newInstance();
+            Field[] fields=this.getClass().getDeclaredFields();
+            for(Field field:fields){
+                field.setAccessible(true);
+                field.set(result,field.get(this));
+            }
+            return result;
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
 }
