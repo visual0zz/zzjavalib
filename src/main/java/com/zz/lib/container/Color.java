51839f39a173f145c673ef6cdf39e4d818ffc8a7
赵治 <zhaozhi@mszq.com>
2023-05-19 15:08:02 +0800

    颜色

--- src/main/java/com/zz/lib/container/Color.java
+++ src/main/java/com/zz/lib/container/Color.java
@@ -1,5 +1,7 @@
 package com.zz.lib.container;
 
+import com.zz.lib.common.CheckUtil;
+
 public class Color {
     int red,green,blue;
     private Color(int red,int green,int blue){
@@ -17,17 +19,13 @@ public class Color {
         return new Color(red,green,blue);
     }
     public static Color ofCMYK(float cyan,float magenta,float yellow,float black){
-        cyan=Math.max(cyan,0);
-        cyan=Math.min(cyan,1);
-        magenta=Math.max(magenta,0);
-        magenta=Math.min(magenta,1);
-        yellow=Math.max(yellow,0);
-        yellow=Math.min(yellow,1);
-        black=Math.max(black,0);
-        black=Math.min(black,1);
+        CheckUtil.mustTrue("input must between 0 and 1"
+                ,cyan>=0&&cyan<=1&&magenta>=0&&magenta<=1&&yellow>=0&&yellow<=1&&black>=0&&black<=1);
         return new Color((int) (255*(1-cyan)*(1-black)), (int) (255*(1-magenta)*(1-magenta)), (int) (255*(1-yellow)*(1-yellow)));
     }
     public static Color ofCMYK(int cyan,int magenta,int yellow,int black){
+        CheckUtil.mustTrue("input must between 0 and 100"
+                ,cyan>=0&&cyan<=100&&magenta>=0&&magenta<=100&&yellow>=0&&yellow<=100&&black>=0&&black<=100);
         return ofCMYK((float) (cyan/100.0), (float) (magenta/100.0), (float) (yellow/100.0), (float) (black/100.0));
     }
 }
--- src/main/java/com/zz/lib/container/MultikeyMap.java
+++ src/main/java/com/zz/lib/container/MultikeyMap.java
@@ -1,7 +1,6 @@
 package com.zz.lib.container;
 
 import com.zz.lib.container.tuple.Tuple;
-import com.zz.lib.container.tuple.Tuple2;
 
 import java.util.HashMap;
 
@@ -9,10 +8,7 @@ import java.util.HashMap;
  * @param <V>value 的数据类型
  */
 public class MultikeyMap<V> {
-    private HashMap<Tuple,V> data;
-    public MultikeyMap(){
-        data=new HashMap<>();
-    }
+    private HashMap<Tuple,V> data=new HashMap<>();
     public V get(Object ... keys){
         return data.get(new Tuple(keys));
     }
